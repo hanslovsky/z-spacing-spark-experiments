70e65ad63263332fc22ed801c89dbcb99b551deb
Philipp Hanslovsky <hanslovskyp@janelia.hhmi.org>
2017-02-02 10:23:27 -0500

    Add experiment for davi toy set

---
+++ src/main/java/de/hanslovsky/zspacing/spark/experiments/DaviToySet.java
@@ -0,0 +1,40 @@
+package de.hanslovsky.zspacing.spark.experiments;
+
+import java.io.IOException;
+import java.lang.invoke.MethodHandles;
+
+import org.apache.log4j.Level;
+import org.apache.log4j.LogManager;
+import org.apache.spark.SparkConf;
+import org.apache.spark.api.java.JavaSparkContext;
+import org.janelia.thickness.KryoSerialization;
+import org.janelia.thickness.ScaleOptions;
+import org.janelia.thickness.ZSpacing;
+
+import loci.formats.FormatException;
+
+public class DaviToySet
+{
+
+	public static void main( final String[] args ) throws FormatException, IOException
+	{
+
+		final SparkConf conf = new SparkConf()
+				.setMaster( "local[*]" )
+				.setAppName( MethodHandles.lookup().lookupClass().getSimpleName() )
+				.set( "spark.serializer", "org.apache.spark.serializer.KryoSerializer" )
+				.set( "spark.kryo.registrator", KryoSerialization.Registrator.class.getName() );
+		final JavaSparkContext sc = new JavaSparkContext( conf );
+
+		LogManager.getRootLogger().setLevel( Level.ERROR );
+
+		final String path = "/data/hanslovskyp/davi_toy_set/z-position-spark-test";
+		final String experiment = "20170201_202843";
+		final String configPath = path + "/" + experiment + "/config.json";
+		final ScaleOptions scaleOptions = ScaleOptions.createFromFile( configPath );
+
+		ZSpacing.run( sc, scaleOptions );
+
+	}
+
+}
